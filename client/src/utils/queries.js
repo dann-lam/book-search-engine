2d7acdfc50eefe4f6a5f042c7d015a149318777c
Daniel Lam <Dann.lam@gmail.com>
2023-06-02 00:51:15 -0700

    Mutations and queries utils added

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,9 +1,20 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
-    query me {
-        user {
-            
-        }
+  query me {
+    me {
+      _id
+      username
+      email
+      bookcount
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
     }
-`
+  }
+`;
