bd1b4c077cc3e577a195f1f8841edbc3f1195629
Daniel Lam <Dann.lam@gmail.com>
2023-06-01 18:39:50 -0700

    TypeDefs added, incomplete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,17 +2,40 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Book {
-    _id: ID!
+    bookId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
   }
 
   type User {
     _id: ID!
     username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
+    me: User
     book: [Book]
-    user(_id: String): [User]
+    user: [User]
+  }
+
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
   }
 
   # type Mutation {
